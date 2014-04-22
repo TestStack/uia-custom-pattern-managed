1599c157f82d157e35991245e140255227daabee
ivan-danilov <van.danilov@gmail.com>
2014-04-23 02:02:32 +0300

    Renamed AttributeExtensions -> ReflectionExtension

--- ManagedUiaCustomizationCore/AttributeDrivenPatternHelpers/ReflectionExtensions.cs
+++ ManagedUiaCustomizationCore/AttributeDrivenPatternHelpers/ReflectionExtensions.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
 
 namespace ManagedUiaCustomizationCore
 {
-    internal static class AttributeExtensions
+    internal static class ReflectionExtensions
     {
         internal static IEnumerable<TA> GetAttributes<TA>(this MemberInfo memberInfo)
         {
-            return memberInfo.GetCustomAttributes(typeof(TA), true).Cast<TA>();
+            return memberInfo.GetCustomAttributes(typeof (TA), true).Cast<TA>();
         }
-        
+
         internal static TA GetAttribute<TA>(this MemberInfo memberInfo)
         {
             return memberInfo.GetAttributes<TA>().FirstOrDefault();
@@ -20,15 +21,15 @@ namespace ManagedUiaCustomizationCore
         internal static IEnumerable<MethodInfo> GetMethodsMarkedWith<TAttribute>(this Type type)
         {
             return from m in type.GetMethods(BindingFlags.Instance | BindingFlags.Public)
-                   where m.GetAttributes<TAttribute>().Any()
-                   select m;
+                where m.GetAttributes<TAttribute>().Any()
+                select m;
         }
 
         internal static IEnumerable<PropertyInfo> GetPropertiesMarkedWith<TAttribute>(this Type type)
         {
             return from m in type.GetProperties(BindingFlags.Instance | BindingFlags.Public)
-                   where m.GetAttributes<TAttribute>().Any()
-                   select m;
+                where m.GetAttributes<TAttribute>().Any()
+                select m;
         }
     }
 }

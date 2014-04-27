83a83dc98773b8fe84d2e5ec796f3d3d4a4c88af
ivan-danilov <van.danilov@gmail.com>
2014-04-28 02:33:21 +0300

    Cleanup

--- ManagedUiaCustomizationCore/Schema/CustomPatternSchemaBase.cs
+++ ManagedUiaCustomizationCore/Schema/CustomPatternSchemaBase.cs
@@ -5,12 +5,12 @@ using Interop.UIAutomationCore;
 namespace ManagedUiaCustomizationCore
 {
     /// <summary>
-    ///     Base class for defining a custom schema.
-    ///     Responsible for defining the minimum info for a custom schema and
-    ///     registering it with UI Automation.
-    ///     This class is not required by UIA and doesn't correspond to anything in UIA;
-    ///     it's a personal preference about the right way to represent what is similar
-    ///     between various schemas and what varies.
+    /// Base class for defining a custom schema.
+    /// Responsible for defining the minimum info for a custom schema and
+    /// registering it with UI Automation.
+    /// This class is not required by UIA and doesn't correspond to anything in UIA;
+    /// it's a personal preference about the right way to represent what is similar
+    /// between various schemas and what varies.
     /// </summary>
     public abstract class CustomPatternSchemaBase
     {
@@ -23,27 +23,27 @@ namespace ManagedUiaCustomizationCore
         // a custom pattern.
 
         /// <summary>
-        ///     The list of properties for this pattern.
+        /// The list of properties for this pattern.
         /// </summary>
         public abstract UiaPropertyInfoHelper[] Properties { get; }
 
         /// <summary>
-        ///     The list of methods for this pattern.
+        /// The list of methods for this pattern.
         /// </summary>
         public abstract UiaMethodInfoHelper[] Methods { get; }
 
         /// <summary>
-        ///     The list of events for this pattern.
+        /// The list of events for this pattern.
         /// </summary>
         public abstract UiaEventInfoHelper[] Events { get; }
 
         /// <summary>
-        ///     The unique ID for this pattern.
+        /// The unique ID for this pattern.
         /// </summary>
         public abstract Guid PatternGuid { get; }
 
         /// <summary>
-        ///     The interface ID for the COM interface for this pattern on the client side.
+        /// The interface ID for the COM interface for this pattern on the client side.
         /// </summary>
         public virtual Guid PatternClientGuid
         {
@@ -51,7 +51,7 @@ namespace ManagedUiaCustomizationCore
         }
 
         /// <summary>
-        ///     The interface ID for the COM interface for this pattern on the provider side.
+        /// The interface ID for the COM interface for this pattern on the provider side.
         /// </summary>
         public virtual Guid PatternProviderGuid
         {
@@ -59,7 +59,7 @@ namespace ManagedUiaCustomizationCore
         }
 
         /// <summary>
-        ///     The programmatic name for this pattern.
+        /// The programmatic name for this pattern.
         /// </summary>
         public abstract string PatternName { get; }
 
@@ -74,13 +74,13 @@ namespace ManagedUiaCustomizationCore
         public abstract Type PatternClientInterface { get; }
 
         /// <summary>
-        ///     An object that implements IUIAutomationPatternHandler to handle
-        ///     dispatching and client-pattern creation for this pattern
+        /// An object that implements IUIAutomationPatternHandler to handle
+        /// dispatching and client-pattern creation for this pattern
         /// </summary>
         public abstract IUIAutomationPatternHandler Handler { get; }
 
         /// <summary>
-        ///     The assigned ID for this pattern.
+        /// The assigned ID for this pattern.
         /// </summary>
         public int PatternId
         {
@@ -88,7 +88,7 @@ namespace ManagedUiaCustomizationCore
         }
 
         /// <summary>
-        ///     The assigned ID for the IsXxxxPatternAvailable property.
+        /// The assigned ID for the IsXxxxPatternAvailable property.
         /// </summary>
         public int PatternAvailablePropertyId
         {
@@ -96,77 +96,73 @@ namespace ManagedUiaCustomizationCore
         }
 
         /// <summary>
-        ///     Helper method to register this pattern.
+        /// Helper method to register this pattern.
         /// </summary>
         public void Register(bool makeAugmentationForWpfPeers = false)
         {
-            if (!_registered)
+            if (_registered) return;
+
+            // Get our pointer to the registrar
+            IUIAutomationRegistrar registrar = new CUIAutomationRegistrarClass();
+
+            // Set up the pattern struct
+            var patternInfo = new UiaPatternInfoHelper(PatternGuid,
+                                                       PatternName,
+                                                       PatternClientGuid,
+                                                       PatternProviderGuid,
+                                                       Handler);
+
+            // Populate it with properties and methods
+            uint index = 0;
+            foreach (var propertyInfo in Properties)
             {
-                // Get our pointer to the registrar
-                IUIAutomationRegistrar registrar = new CUIAutomationRegistrarClass();
-
-                // Set up the pattern struct
-                var patternInfo = new UiaPatternInfoHelper(
-                    PatternGuid,
-                    PatternName,
-                    PatternClientGuid,
-                    PatternProviderGuid,
-                    Handler
-                    );
-
-                // Populate it with properties and methods
-                uint index = 0;
-                foreach (var propertyInfo in Properties)
-                {
-                    patternInfo.AddProperty(propertyInfo);
-                    propertyInfo.Index = index++;
-                    if (propertyInfo.SupportsDispatch)
-                        _members[propertyInfo.Index] = propertyInfo;
-                }
-                foreach (var methodInfo in Methods)
-                {
-                    patternInfo.AddMethod(methodInfo);
-                    methodInfo.Index = index++;
-                    if (methodInfo.SupportsDispatch)
-                        _members[methodInfo.Index] = methodInfo;
-                }
-
-                // Add the events, too, although they are not indexed
-                foreach (var eventInfo in Events)
-                {
-                    patternInfo.AddEvent(eventInfo);
-                }
-
-                // Register the pattern
-                var patternData = patternInfo.Data;
-
-                // Call register pattern
-                int[] propertyIds = new int[patternData.cProperties];
-                int[] eventIds = new int[patternData.cEvents];
-                registrar.RegisterPattern(
-                    ref patternData,
-                    out _patternId,
-                    out _patternAvailablePropertyId,
-                    patternData.cProperties,
-                    propertyIds,
-                    patternData.cEvents,
-                    eventIds);
-
-                // Write the property IDs back
-                for (uint i = 0; i < propertyIds.Length; ++i)
-                {
-                    Properties[i].PropertyId = propertyIds[i];
-                }
-                for (var i = 0; i < eventIds.Length; ++i)
-                {
-                    Events[i].EventId = eventIds[i];
-                }
-
-                if (makeAugmentationForWpfPeers)
-                    AutomationPeerAugmentationHelper.Register(this);
-
-                _registered = true;
+                patternInfo.AddProperty(propertyInfo);
+                propertyInfo.Index = index++;
+                if (propertyInfo.SupportsDispatch)
+                    _members[propertyInfo.Index] = propertyInfo;
             }
+            foreach (var methodInfo in Methods)
+            {
+                patternInfo.AddMethod(methodInfo);
+                methodInfo.Index = index++;
+                if (methodInfo.SupportsDispatch)
+                    _members[methodInfo.Index] = methodInfo;
+            }
+
+            // Add the events, too, although they are not indexed
+            foreach (var eventInfo in Events)
+            {
+                patternInfo.AddEvent(eventInfo);
+            }
+
+            // Register the pattern
+            var patternData = patternInfo.Data;
+
+            // Call register pattern
+            int[] propertyIds = new int[patternData.cProperties];
+            int[] eventIds = new int[patternData.cEvents];
+            registrar.RegisterPattern(ref patternData,
+                                      out _patternId,
+                                      out _patternAvailablePropertyId,
+                                      patternData.cProperties,
+                                      propertyIds,
+                                      patternData.cEvents,
+                                      eventIds);
+
+            // Write the property IDs back
+            for (uint i = 0; i < propertyIds.Length; ++i)
+            {
+                Properties[i].PropertyId = propertyIds[i];
+            }
+            for (var i = 0; i < eventIds.Length; ++i)
+            {
+                Events[i].EventId = eventIds[i];
+            }
+
+            if (makeAugmentationForWpfPeers)
+                AutomationPeerAugmentationHelper.Register(this);
+
+            _registered = true;
         }
 
         public ISchemaMember GetMemberByIndex(uint index)

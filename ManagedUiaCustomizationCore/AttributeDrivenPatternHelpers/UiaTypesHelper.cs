b7a5886ea354b6dc8f57127061b836e03bcab56f
ivan-danilov <van.danilov@gmail.com>
2014-05-02 03:21:41 +0300

    Simplified UIA type calculation a bit, plus made every possible IRawElementProviderSimple type viable (comparing by type GUID)

--- ManagedUiaCustomizationCore/AttributeDrivenPatternHelpers/UiaTypesHelper.cs
+++ ManagedUiaCustomizationCore/AttributeDrivenPatternHelpers/UiaTypesHelper.cs
@@ -11,46 +11,30 @@ namespace ManagedUiaCustomizationCore
         private static readonly Dictionary<Type, UIAutomationType> _typeMapping
             = new Dictionary<Type, UIAutomationType>
               {
-                  {typeof (int), UIAutomationType.UIAutomationType_Int},
-                  {typeof (bool), UIAutomationType.UIAutomationType_Bool},
-                  {typeof (string), UIAutomationType.UIAutomationType_String},
-                  {typeof (double), UIAutomationType.UIAutomationType_Double},
-
-                  // We want to support at least UIAComWrapper which uses Interop.AutomationClient,
-                  // this code which uses Interop.UIAutomationCore and WPF's UIAutomationProvider.
-                  // Actually it'd be better to ask type through IUnknown if it implements interface
-                  // with required IID, but for now such simplified mapping would be enough.
-                  {typeof (IRawElementProviderSimple), UIAutomationType.UIAutomationType_Element},
-                  {typeof (UIAutomationClient.IRawElementProviderSimple), UIAutomationType.UIAutomationType_Element},
-                  {typeof (System.Windows.Automation.Provider.IRawElementProviderSimple), UIAutomationType.UIAutomationType_Element},
+                  {typeof(int), UIAutomationType.UIAutomationType_Int},
+                  {typeof(bool), UIAutomationType.UIAutomationType_Bool},
+                  {typeof(string), UIAutomationType.UIAutomationType_String},
+                  {typeof(double), UIAutomationType.UIAutomationType_Double},
               };
 
-        private static readonly Dictionary<Type, UIAutomationType> _outTypeMapping
-            = new Dictionary<Type, UIAutomationType>
-              {
-                  {typeof (int), UIAutomationType.UIAutomationType_OutInt},
-                  {typeof (bool), UIAutomationType.UIAutomationType_OutBool},
-                  {typeof (string), UIAutomationType.UIAutomationType_OutString},
-                  {typeof (double), UIAutomationType.UIAutomationType_OutDouble},
-                  {typeof (IRawElementProviderSimple), UIAutomationType.UIAutomationType_OutElement},
-                  {typeof (UIAutomationClient.IRawElementProviderSimple), UIAutomationType.UIAutomationType_OutElement},
-                  {typeof (System.Windows.Automation.Provider.IRawElementProviderSimple), UIAutomationType.UIAutomationType_OutElement},
-              };
-
-        public static UIAutomationType TypeToAutomationType(Type propertyType)
+        public static UIAutomationType TypeToAutomationType(Type type)
         {
+            if (IsElementOnServerSide(type))
+                return UIAutomationType.UIAutomationType_Element;
             UIAutomationType res;
-            if (_typeMapping.TryGetValue(propertyType, out res))
+            if (_typeMapping.TryGetValue(type, out res))
                 return res;
             throw new NotSupportedException("Provided type is not supported");
         }
 
-        public static UIAutomationType TypeToOutAutomationType(Type propertyType)
+        public static UIAutomationType TypeToOutAutomationType(Type type)
         {
-            UIAutomationType res;
-            if (_outTypeMapping.TryGetValue(propertyType, out res))
-                return res;
-            throw new NotSupportedException("Provided type is not supported");
+            return TypeToAutomationType(type) | UIAutomationType.UIAutomationType_Out;
+        }
+
+        public static bool IsElementOnServerSide(Type type)
+        {
+            return type.GUID == typeof(IRawElementProviderSimple).GUID;
         }
 
         public static bool IsInType(UIAutomationType type)
